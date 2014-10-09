6a987377fe74dc04db5163437f515da3d9521470
Michel Kuhlmann <michel@kuhlmanns.info>
2014-10-09 10:07:26 +0200

    fix sms writer

--- src/DevSurf/Writers/Sms2dm.hs
+++ src/DevSurf/Writers/Sms2dm.hs
@@ -2,21 +2,22 @@ module DevSurf.Writers.Sms2dm (renderSms2dm) where
 import DevSurf.Types
 import Data.HashMap.Lazy (toList)
 import Data.List (intersperse)
+import DevSurf.Writers.Utils (toOrdList)
 
 renderSms2dm :: IndexedFaceSet -> String
 renderSms2dm (IndexedFaceSet fs vs) = unlines . concat $ [
     ["MESH2D"]
-   , map renderFace (toList fs)
-   , map renderVertex (toList vs)
+   , map renderFace (toOrdList fs)
+   , map renderVertex (toOrdList vs)
    ]
 
 renderFace :: (Int,IndexedFace) -> String
 renderFace (i, (v1,v2,v3)) =
-    "E3T " ++ " "++ show i ++ (showIntList [v1,v2,v3])
+    "E3T " ++ " "++ show i ++ " " ++ (showIntList [v1,v2,v3]) ++ " 1"
 
 renderVertex :: (Int,Vertex) -> String
 renderVertex (i,(x,y,z)) =
-    "ND " ++ show i ++ (showDoubleList [x,y,z])
+    "ND " ++ show i ++ " " ++ (showDoubleList [x,y,z])
 
 showDoubleList :: [Double] -> String
 showDoubleList = unwords . intersperse " " . map show
--- src/DevSurf/Writers/Utils.hs
+++ src/DevSurf/Writers/Utils.hs
@@ -7,5 +7,11 @@ import Data.Maybe (fromJust)
 ordElems :: HashMap Int a -> [a]
 ordElems m = map (\k -> fromJust $ lookup k m) (sort . keys $ m)
 
+toOrdList :: HashMap Int a -> [(Int,a)]
+toOrdList m = zip ks vs
+    where
+        ks = sort. keys $ m
+        vs = map (\k -> fromJust $ lookup k m) ks
+
 round2Digit :: Double -> Double
 round2Digit x = fromIntegral (round (x * 100)) / 100
